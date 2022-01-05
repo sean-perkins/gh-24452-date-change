b799f633366f5d8b3e1f54f721f0464d314015ec
Sean Perkins <sean@ionic.io>
2022-01-05 14:36:01 -0500

    chore(): reproduce issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  dateValue = new Date().toISOString();
+
+  form = new FormGroup({
+    date: new FormControl(new Date().toISOString())
+  });
+
+  onNgModelDateChange(ev) {
+    console.log('[ionChange -- ngModel]', ev);
+  }
+
+  onFormControlDateChange(ev) {
+    console.log('[ionChange -- formControl]', ev);
+  }
 }
